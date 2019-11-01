88b6756139f581bb239498dd8acf95deee743ed1
mohamedsaad <mahameds19999@gmail.com>
2019-11-01 04:15:37 +0200

    Updates

---
+++ precache-manifest.93f3bd501d57ab9d92e32b5138a745e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a3fd36eb9743fab5c3619532a09d59",
+    "url": "/country-api/index.html"
+  },
+  {
+    "revision": "bbb760f097c3d8570bc6",
+    "url": "/country-api/static/css/main.4e3d22a4.chunk.css"
+  },
+  {
+    "revision": "c735fdaaa54c6974967c",
+    "url": "/country-api/static/js/2.43e0e06a.chunk.js"
+  },
+  {
+    "revision": "bbb760f097c3d8570bc6",
+    "url": "/country-api/static/js/main.2a173d69.chunk.js"
+  },
+  {
+    "revision": "30e16d8aa86e9b72a5f3",
+    "url": "/country-api/static/js/runtime-main.db6f9b4d.js"
+  }
+]);
